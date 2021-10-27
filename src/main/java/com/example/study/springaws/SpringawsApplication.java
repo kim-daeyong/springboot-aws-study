671b3097cf84955a5cfdf18e0ce6857d08b6b54e
kim-daeyong <kdy2353@gmail.com>
2021-10-27 14:14:00 +0900

    test

--- src/main/java/com/example/study/springaws/SpringawsApplication.java
+++ src/main/java/com/example/study/springaws/SpringawsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA auditing 활성화
+// @EnableJpaAuditing // JPA auditing 활성화
 @SpringBootApplication
 // @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringawsApplication {
--- src/test/java/com/example/study/springaws/controller/HelloControllerTest.java
+++ src/test/java/com/example/study/springaws/controller/HelloControllerTest.java
@@ -3,44 +3,52 @@ package com.example.study.springaws.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.study.springaws.config.auth.SecurityConfig;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.hamcrest.Matchers.is;
 
 // @SpringBootTest
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void hello리턴() throws Exception {
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
-            .andExpect(status().isOk())
-            .andExpect(content().string(hello));
-
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void hellodto리턴() throws Exception{
+    public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
         mvc.perform(get("/hello/dto")
-                    .param("name", name)
-                    .param("amount", String.valueOf(amount))
-                    ).andExpect(status().isOk())
-                        .andExpect(jsonPath("$.name", is(name)))
-                        .andExpect(jsonPath("$.amount", is(amount)))
-                        .andDo(print());
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
-
 }
